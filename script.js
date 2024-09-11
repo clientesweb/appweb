345ca583b8d9a096b02365ef39e99c8b4751ad32
clientesweb <alexisifran32@gmail.com>
2024-09-11 10:39:49 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -20,6 +20,10 @@ menuIcon.onclick = function(){
         '5Ia0gTq0kq8',
         '9Nr3SMzGuoU',
         '-8OGNYFZJtM'
+        'QlQdYxAT_fk'
+        '5E24fAubqm0'
+        'X1IVvJuR8aA'
+        'S3oYxa0WHlI'
         // Agrega más IDs de Shorts aquí
     ];
 
