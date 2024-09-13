1bf49e442fe2ade969ac08de0c442cf8db7fd76e
clientesweb <alexisifran32@gmail.com>
2024-09-12 21:01:12 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var menuIcon = document.querySelector('.menu-icon');
-    var sidebar = document.querySelector('.sidebar');
-
-    console.log('menuIcon:', menuIcon); // Para verificar que el icono del menú se está seleccionando
-    console.log('sidebar:', sidebar);   // Para verificar que la barra lateral se está seleccionando
-
-    // Al hacer clic en el icono del menú, mostrar/ocultar la barra lateral
-    menuIcon.addEventListener('click', function() {
-        console.log('Menu icon clicked'); // Para confirmar que el clic está siendo registrado
-        sidebar.classList.toggle('show');
-        console.log('Sidebar classes:', sidebar.classList); // Para ver las clases que tiene el sidebar
-    });
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('nav-menu').classList.toggle('active');
 });
  document.addEventListener('DOMContentLoaded', () => {
     // Lista de IDs de YouTube Shorts
