55603e44d1a40492067d6138e995417379fb6f36
clientesweb <alexisifran32@gmail.com>
2024-09-20 18:42:24 -0300

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,61 @@
+const CACHE_NAME = 'cache-v1';
+const RESOURCES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/images/Icon192x192.png',
+  '/images/Icon512x512.png',
+  '/images/logo.svg',
+  '/images/logo1.png',
+  '/images/logo2.png',
+  '/images/logo3.png',
+  '/images/logo4.png',
+  '/image1.jpg',
+  '/image2.jpg',
+  '/image3.jpg',
+  '/image4.jpg',
+  '/image5.jpg',
+  '/image6.jpg'
+];
+
+// Instalación del Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('cache-v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/images/Icon192x192.png',
-        '/images/Icon512x512.png',
-        '/images/logi.svg',
-        '/images/logo1.png',
-        '/images/logo2.png',
-        '/images/logo3.png',
-        '/images/logo4.png',
-        '/image1.jpg',
-        '/image2 (1).jpg',
-        '/image3.jpg',
-        '/image4.jpg',
-        '/image5.jpg',
-        '/image6.jpg'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return Promise.all(
+        RESOURCES_TO_CACHE.map((resource) => cache.add(resource))
+      );
     })
   );
 });
 
+// Activación del Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Eliminar cachés antiguos
+          }
+        })
+      );
+    })
+  );
+});
+
+// Respuesta a las solicitudes
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request).then((fetchResponse) => {
-        // Optionally add fetched resources to cache
-        return caches.open('cache-v1').then((cache) => {
+        // Almacenar los recursos solicitados en caché
+        return caches.open(CACHE_NAME).then((cache) => {
           cache.put(event.request, fetchResponse.clone());
           return fetchResponse;
+        }).catch((error) => {
+          console.error('Error al almacenar en caché:', error);
         });
       });
     })
